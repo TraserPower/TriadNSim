abd6fd4a6e364d784e4fdc5b2f92b18c365717a2
aleshka-f2000 <aleshka-f2000@yandex.ru>
2023-05-23 19:54:03 +0500

    Добавьте файлы проекта.

---
+++ TriadCompiler/Обработка типов/Типы/Переменная/VarListType.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TriadCompiler
+{
+    /// <summary>
+    /// Тип данных список
+    /// </summary>
+    internal class VarListType : IExprType
+    {
+        /// <summary>
+        /// Базовый тип переменной
+        /// </summary>
+        public TypeCode Code { get; set; }
+
+        /// <summary>
+        /// Признак spy-объекта
+        /// </summary>
+        public bool IsSpyObject { get; set; } = false;
+
+        /// <summary>
+        /// Имя переменной
+        /// </summary>
+        public string Name { get; set; }
+
+
+        public VarListType(TypeCode typeCode)
+        {
+            this.Code = typeCode;
+        }
+
+        public VarListType(TypeCode typeCode, string varName) : this(typeCode)
+        {
+            this.Name = varName;
+        }
+
+        public IExprType Clone()
+        {
+            return  new VarListType(this.Code, this.Name);
+        }
+    }
+}
---
+++ TriadCore/Общее/Функции/ListFunctions.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TriadCore
+{
+    /// <summary>
+    /// Функции для работы со списками
+    /// </summary>
+    public static class ListFunctions
+    {
+        /// <summary>
+        /// Получение количества элементов списка
+        /// </summary>
+        /// <param name="list">список</param>
+        /// <returns>Количество элементов списка</returns>
+        public static int ListSize(IList list)
+        {
+            return list.Count;
+        }
+
+        /// <summary>
+        /// Добавление элемента в конец списка
+        /// </summary>
+        /// <param name="list">список</param>
+        /// <param name="item">добавляемый элемент</param>
+        /// <returns></returns>
+        public static void ListAdd(IList list, object item)
+        {
+            list.Add(item);
+        }
+
+        /// <summary>
+        /// Удаление первого вхождения элемента
+        /// </summary>
+        /// <param name="list">список</param>
+        /// <param name="item">удаляемый элемент</param>
+        /// <returns></returns>
+        public static void ListRemove(IList list, object item)
+        {
+            list.Remove(item);
+        }
+
+        /// <summary>
+        /// Добавление элемента в позицию index
+        /// </summary>
+        /// <param name="list">список</param>
+        /// <param name="index">индекс</param>
+        /// <param name="item">добавляемый элемент</param>
+        /// <returns></returns>
+        public static void ListInsert(IList list, int index, object item)
+        {
+            list.Insert(index, item);
+        }
+
+        /// <summary>
+        /// Удаление элемента по индексу
+        /// </summary>
+        /// <param name="list">список</param>
+        /// <param name="index">индекс</param>
+        /// <returns></returns>
+        public static void ListRemoveAt(IList list, int index)
+        {
+            list.RemoveAt(index);
+        }
+    }
+}
